9568cf65b858f4d246e2522c23d9dc141b1d3110
michaelnardellipriv <michael06.nardelli@gmail.com>
2026-01-29 11:17:39 +0100

    aggioranmento intefaccia race

--- src/types/race.ts
+++ src/types/race.ts
@@ -1,14 +1,34 @@
-
-import type { Circuit } from './circuit';
-
-/**
- * Type definition per Race F1
- */
+// types/race.ts
+import type { Circuit } from "@/types/circuit";
+import type { Driver } from "@/types/driver";
+import type { Team } from "@/types/team";
 export interface RaceStats {
-  round: string;
-  date: string;
-  time: string;
   raceId: string;
+  championshipId: string;
   raceName: string;
+  round: number;
+  url: string;
+  laps: number;
+  
+  schedule: {
+    race: { date: string; time: string; };
+    qualy: { date: string; time: string; };
+    fp1: { date: string; time: string; };
+    fp2: { date: string; time: string; };
+    fp3: { date: string; time: string; };
+    sprintQualy: { date: string | null; time: string | null; };
+    sprintRace: { date: string | null; time: string | null; };
+  };
+  
+  fast_lap: {
+    fast_lap: string;
+    fast_lap_driver_id: string;
+    fast_lap_team_id: string;
+  };
+  
   circuit: Circuit;
+  
+  
+  winner: Driver;  
+  teamWinner: Team; 
 }
